5df171f2fc8c14d340d14dd9e064e24714a6631f
higg <higginshiggins2@protonmail.com>
2022-02-20 16:16:44 -0800

    silenced ffplay text output and added pomodoro counter

--- pomodoro-timer.py
+++ pomodoro-timer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import time, os
 
+n = 0
 while(1):
-    minutes = input("minutes (float): ")
+    minutes = input("[" + str(n) + "] minutes: ")
     time.sleep(float(minutes) * 60)
     for y in minutes:
-        os.system("ffplay -nodisp -autoexit ~/work_a*/git*/pomo*/res/"+y+"-1.wav")
+        os.system("ffplay -nodisp -autoexit ~/work_a*/git*/pomo*/res/"+y+"-1.wav 1>/dev/null 2>/dev/null")
+    n += 1
