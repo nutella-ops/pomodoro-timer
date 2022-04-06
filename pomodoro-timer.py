b7dc1ad3403a981d4d54c8cc2c3c895c092a83ff
higg <higginshiggins2@protonmail.com>
2022-04-05 18:40:43 -0700

    somehow does not respond to the IF statement

--- pomodoro-timer.py
+++ pomodoro-timer.py
@@ -3,8 +3,12 @@ import time, os
 
 n = 0
 while(1):
-    minutes = input("[" + str(n) + "] minutes: ")
-    time.sleep(float(minutes) * 60)
-    for y in minutes:
+    userInput = input("[" + str(n) + "] minutes: ")
+    minutes = float(userInput)
+    time.sleep(minutes * 60)
+    for y in userInput:
         os.system("ffplay -nodisp -autoexit ~/work_a*/git*/pomo*/res/"+y+"-1.wav 1>/dev/null 2>/dev/null")
-    n += 1
+    if userInput == 0.5 or 0.10 or 0.15 or 0.20:
+        n += 0
+    else:
+        n += 1
