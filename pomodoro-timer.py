3a9ebbbcdb494f7848b721f2fc6bd0d7e8a6056c
higg <higginshiggins2@protonmail.com>
2022-04-06 03:38:46 -0700

    added current time data

--- pomodoro-timer.py
+++ pomodoro-timer.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 import time, os
 
+def day():
+    print(time.ctime()[4:10])
+
+def clk():
+    print(time.ctime()[11:19])
+
 n = 0
+day()
+print("-----------")
 while(1):
+    clk()
     userInput = input("[" + str(n) + "] minutes: ")
     minutes = float(userInput)
     time.sleep(minutes * 60)
     for y in userInput:
         os.system("ffplay -nodisp -autoexit ~/work_a*/git*/pomo*/res/"+y+"-1.wav 1>/dev/null 2>/dev/null")
-    if userInput == 0.5 or 0.10 or 0.15 or 0.20:
-        n += 0
-    else:
-        n += 1
+    n += 1
