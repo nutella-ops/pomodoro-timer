0fd886a21c0aa45a6eebc49f4117d8cf9b82d94e
higg <higginshiggins2@protonmail.com>
2022-04-07 12:41:13 -0700

    outputs time interval and writes to file based on current date

--- pomodoro-timer.py
+++ pomodoro-timer.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 import time, os
 
-def day():
-    print(time.ctime()[4:10])
-
-def clk():
-    print(time.ctime()[11:19])
-
-file = time.ctime()[4:19]
+day = time.ctime()[4:10]
 
 n = 0
-day()
-print("-----------")
 while(1):
-    clk()
     userInput = input("[" + str(n) + "] minutes: ")
     minutes = float(userInput)
+
+    startTime = time.ctime()[11:16]
     time.sleep(minutes * 60)
+    stopTime = time.ctime()[11:16]
+
+    logEntry = startTime + " -- " + stopTime
+    print(day + " " + logEntry)
+
     for y in userInput:
         os.system("ffplay -nodisp -autoexit ~/work_a*/git*/pomo*/res/"+y+"-1.wav 1>/dev/null 2>/dev/null")
     n += 1
-    os.system("echo " + time.ctime()[4:19] + " >> /tmp/" + "'" + file + "'" + ".txt")
+    os.system("echo " + logEntry + " >> /tmp/" + "'" + day + "'" + ".txt")
