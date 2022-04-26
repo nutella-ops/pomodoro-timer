993fb40fc358a5a9aa1d5407d596b0b1035a880e
higg <higginshiggins2@protonmail.com>
2022-04-26 10:35:23 -0700

    WIP: next; add prompt to save on interrupt

--- pomodoro-timer.py
+++ pomodoro-timer.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 import time, os
 
-def dayCheck():
-    if (sysTime[2] == day):
-        os.system("echo " + logPom + " >> /tmp/" + fileName)
-    else:
-        print("new day")
-
 sysTime = time.ctime().split()
 day = sysTime[2]
 month = sysTime[1]
 dayLow = (month + day).lower()
 fileName = dayLow +".txt"
 dayMonth = month + " " + day
-
+logHeader = dayMonth
 os.system("echo " + logHeader + " >> /tmp/" + fileName)
 
-n = 0
+def dayCheck():
+    if (sysTime[2] == day):
+        os.system("echo " + logPom + " >> /tmp/" + fileName)
+    else:
+        os.system("echo " + logHeader + " >> /tmp/" + fileName)
+        os.system("echo " + logPom + " >> /tmp/" + fileName)
+
+pom = 0
 while(1):
-    userInput = input("[" + str(n) + "] minutes: ")
+    userInput = input("[" + str(pom) + "] minutes: ")
     minutes = float(userInput)
 
     startTime = sysTime[3][0:5]
@@ -29,9 +30,4 @@ while(1):
     print(logHeader + ", " + logPom)
 
     os.system("ffplay -nodisp -autoexit $(find ~ -name 'g.wav' 2>/dev/null) 2>/dev/null")
-    n += 1
-
-    if
-    os.system("echo " + logHeader + " >> /tmp/" + fileName)
-
-
+    pom += 1
