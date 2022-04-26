29d8ecbfc08884fd3e16074b892917a545c34e54
higg <higginshiggins2@protonmail.com>
2022-04-26 10:01:25 -0700

    WIP: date recognition

--- pomodoro-timer.py
+++ pomodoro-timer.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
 import time, os
 
-day = time.ctime()[4:10]
+def dayCheck():
+    if (sysTime[2] == day):
+        os.system("echo " + logPom + " >> /tmp/" + fileName)
+    else:
+        print("new day")
+
+sysTime = time.ctime().split()
+day = sysTime[2]
+month = sysTime[1]
+dayLow = (month + day).lower()
+fileName = dayLow +".txt"
+dayMonth = month + " " + day
+
+os.system("echo " + logHeader + " >> /tmp/" + fileName)
 
 n = 0
 while(1):
     userInput = input("[" + str(n) + "] minutes: ")
     minutes = float(userInput)
 
-    startTime = time.ctime()[11:16]
+    startTime = sysTime[3][0:5]
     time.sleep(minutes * 60)
-    stopTime = time.ctime()[11:16]
+    stopTime = sysTime[3][0:5]
 
-    logEntry = startTime + " -- " + stopTime
-    print(day + " " + logEntry)
+    logPom = startTime + ", " + stopTime
+    print(logHeader + ", " + logPom)
 
     os.system("ffplay -nodisp -autoexit $(find ~ -name 'g.wav' 2>/dev/null) 2>/dev/null")
     n += 1
-    os.system("echo " + logEntry + " >> /tmp/" + "'" + day + "'" + ".txt")
+
+    if
+    os.system("echo " + logHeader + " >> /tmp/" + fileName)
+
+
