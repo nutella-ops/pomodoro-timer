64a604522e8aee30fd522f3ac688ecd8871bd46f
higg <higginshiggins2@protonmail.com>
2022-05-07 08:00:12 -0700

    fixed static dates due to timestamps as variables

--- pomodoro-timer.py
+++ pomodoro-timer.py
@@ -6,33 +6,26 @@ day = sysTime[2]
 month = sysTime[1]
 dayLow = (month + day).lower()
 fileName = dayLow +".txt"
-dayMonth = month + " " + day
-logHeader = dayMonth
+logHeader = month + " " + time.ctime().split()[2]
 os.system("echo " + logHeader + " >> /tmp/" + fileName)
 
 def dayCheck():
-    if (sysTime[2] == day):
+    if (time.ctime().split()[2] == day):
         os.system("echo " + logPom + " >> /tmp/" + fileName)
     else:
         os.system("echo " + logHeader + " >> /tmp/" + fileName)
         os.system("echo " + logPom + " >> /tmp/" + fileName)
 
 pom = 0
-# try:
 while(1):
     userInput = input("[" + str(pom) + "] minutes: ")
     minutes = float(userInput)
 
-    startTime = sysTime[3][0:5]
+    startTime = time.ctime().split()[3][0:5]
     time.sleep(minutes * 60)
-    stopTime = sysTime[3][0:5]
+    stopTime = time.ctime().split()[3][0:5]
     os.system("ffplay -nodisp -autoexit $(find ~ -name 'g.wav' 2>/dev/null) 2>/dev/null")
     logPom = startTime + ", " + stopTime
     print(logHeader + ", " + logPom)
     dayCheck()
     pom += 1
-# except KeyboardInterrupt:
-#    response = input("Save your data?: ") 
-#     if (response.lower()[0] == y):
-#         os.system("mkdir ~/Documents/poms")
-#         os.system("mv /tmp/" + month.lower() + "*.txt ~/Documents/poms"
